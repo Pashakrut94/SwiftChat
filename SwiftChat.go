28d8568af9be3aaa9a4fa765f8422447d070ea38
Pavel <pashakrut94@gmail.com>
2019-11-30 18:16:12 +0300

    release v1

---
+++ SwiftChat.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var count int
+var messages []Message
+
+func Counter(w http.ResponseWriter, r *http.Request) {
+	count++
+	fmt.Fprintf(w, "%d", count)
+}
+
+type Message struct {
+	Receiver string
+	Text     string
+}
+
+func MakeMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create new message")
+	text := r.URL.Query().Get("text")
+	receiver := r.URL.Query().Get("receiver")
+
+	msg := Message{Receiver: receiver, Text: text}
+	messages = append(messages, msg)
+
+}
+
+func main() {
+	http.HandleFunc("/Counter", Counter) //endPoint
+
+	http.HandleFunc("/MakeMessage", MakeMessage)
+	http.HandleFunc("/PrintMessage", PrintMessage)
+
+	fmt.Println("Server starts at :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+//2 ендпоинта : один печатает mesgs, второй принимает mesgs
+// mesgs будет как параметр с полями: (кому и текст)
+// MakeMessage?text=Privet&receiver=Pasha
+
+func PrintMessage(w http.ResponseWriter, r *http.Request) {
+	receiver := r.URL.Query().Get("receiver")
+	for _, msg := range messages {
+		if receiver != "" && receiver != msg.Receiver {
+			continue
+		}
+		fmt.Fprintf(w, "Message \"%s\" sent to  %s\n", msg.Text, msg.Receiver)
+	}
+}
+
+// FastChat repositorii chat pod moim imenem, dobavit Ilya, vilozhit tyda pull-request, zapushit tuda
+// залить свой код на репозиторий, гит комит, гит пул
